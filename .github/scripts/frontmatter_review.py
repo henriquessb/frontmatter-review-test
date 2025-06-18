29b60328461fb3ac0398cc27c161563d4fce2878
Henrique Balbino <henrique.balbino@vtex.com>
2025-06-18 11:28:14 -0300

    Change rule to get filename

--- .github/scripts/frontmatter_review.py
+++ .github/scripts/frontmatter_review.py
@@ -82,7 +82,7 @@ for f in changed_files:
                     if not (isinstance(value, str) and re.fullmatch(r'[a-z0-9\-]+', value)):
                         errors.append(f"'slug' must contain only lowercase letters, numbers, and hyphens.")
                         error_found = True
-                    if value != f.filename.split('/')[-1].replace('.md', '').replace('.mdx', ''):
+                    if value != f.filename.split('/')[-1].replace('.mdx', '').replace('.md', ''):
                         errors.append(f"'slug' must match the filename without extension.")
                         error_found = True
                     continue
